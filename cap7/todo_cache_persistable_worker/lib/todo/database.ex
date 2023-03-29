a0789d3b2d78414d1828698db2dcab85ec3a0169
André Rodrigues <andre.rodrigues@agencian1.com.br>
2023-03-28 22:06:27 -0400

    7.3.5 Exercise: pooling and synchronizing

---
+++ cap7/todo_cache_persistable_worker/lib/todo/database.ex
@@ -0,0 +1,48 @@
+defmodule Todo.Database do
+  use GenServer
+
+  @db_folder "./persist"
+
+  # client process
+
+  def start do
+    GenServer.start(__MODULE__, nil, name: __MODULE__)
+  end
+
+  def store(key, data) do
+    key
+    |> choose_worker()
+    |> Todo.DatabaseWorker.store(key, data)
+  end
+
+  def get(key) do
+    key
+    |> choose_worker()
+    |> Todo.DatabaseWorker.get(key)
+  end
+
+  defp choose_worker(key) do
+    GenServer.call(__MODULE__, {:choose_worker, key})
+  end
+
+  # server process
+
+  @impl GenServer
+  def init(_) do
+    File.mkdir_p!(@db_folder)
+    {:ok, start_workers()}
+  end
+
+  @impl GenServer
+  def handle_call({:choose_worker, key}, _caller, workers) do
+    worker_key = :erlang.phash2(key, 3)
+    {:reply, Map.get(workers, worker_key), workers}
+  end
+
+  defp start_workers() do
+    for index <- 0..2, into: %{} do
+      {:ok, pid} = Todo.DatabaseWorker.start(@db_folder)
+      {index, pid}
+    end
+  end
+end
---
+++ cap7/todo_cache_persistable_worker/lib/todo/server.ex
@@ -0,0 +1,40 @@
+defmodule Todo.Server do
+  use GenServer
+
+  # client process
+
+  def start(list_name) do
+    GenServer.start(__MODULE__, list_name)
+  end
+
+  def add_entry(todo_server, new_entry) do
+    GenServer.cast(todo_server, {:add_entry, new_entry})
+  end
+
+  def entries(todo_server, date) do
+    GenServer.call(todo_server, {:entries, date})
+  end
+
+  # server process
+
+  @impl GenServer
+  def init(list_name) do
+    {:ok, {list_name, Todo.Database.get(list_name) || Todo.List.new()}}
+  end
+
+  @impl GenServer
+  def handle_cast({:add_entry, new_entry}, {list_name, state}) do
+    new_state = Todo.List.add_entry(state, new_entry)
+    Todo.Database.store(list_name, new_state)
+    {:noreply, {list_name, new_state}}
+  end
+
+  @impl GenServer
+  def handle_call({:entries, date}, _, {list_name, state}) do
+    {
+      :reply,
+      Todo.List.entries(state, date),
+      {list_name, state}
+    }
+  end
+end
