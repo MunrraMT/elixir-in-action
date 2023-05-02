c2d93ca1d49519f73507ac48d50f113591e5584a
André Rodrigues <andre.rodrigues@agencian1.com.br>
2023-05-02 15:33:27 -0400

    9.2.2 Dynamic supervision

--- cap9/supervised_todo_cache/lib/todo/cache.ex
+++ cap9/supervised_todo_cache/lib/todo/cache.ex
@@ -1,15 +1,30 @@
 defmodule Todo.Cache do
   use GenServer
-
   # client process
 
   def start_link(_) do
     IO.puts("starting to-do cache!")
-    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+
+    DynamicSupervisor.start_link(
+      name: __MODULE__,
+      strategy: :one_for_one
+    )
   end
 
   def server_process(todo_list_name) do
     GenServer.call(__MODULE__, {:server_process, todo_list_name})
+  end
+
+  def child_spec(_arg) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, []},
+      type: :supervisor
+    }
+  end
+
+  defp start_child(todo_list_name) do
+    DynamicSupervisor.start_child(__MODULE__, {Todo.Server, todo_list_name})
   end
 
   # server process
