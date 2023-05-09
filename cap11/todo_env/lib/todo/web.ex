0d3b2d29351b2d85a9843ff7486eec491e9a10d1
MunrraMT <dev.armsantos@gmail.com>
2023-05-08 21:29:24 -0400

    11.4.1 Application environment

---
+++ cap11/todo_env/lib/todo/web.ex
@@ -0,0 +1,59 @@
+defmodule Todo.Web do
+  use Plug.Router
+
+  plug(:match)
+  plug(:dispatch)
+
+  def child_spec(_opts) do
+    Plug.Cowboy.child_spec(
+      scheme: :http,
+      options: [
+        port: Application.fetch_env!(:todo, :http_port)
+      ],
+      plug: __MODULE__
+    )
+  end
+
+  get "/" do
+    send_resp(conn, 200, "Welcome")
+  end
+
+  post "/add_entry" do
+    conn = Plug.Conn.fetch_query_params(conn)
+    list_name = Map.fetch!(conn.params, "list")
+    title = Map.fetch!(conn.params, "title")
+    date = Date.from_iso8601!(Map.fetch!(conn.params, "date"))
+
+    list_name
+    |> Todo.Cache.server_process()
+    |> Todo.Server.add_entry(%{title: title, date: date})
+
+    conn
+    |> Plug.Conn.put_resp_content_type("text/plain")
+    |> Plug.Conn.send_resp(200, "Ok!")
+  end
+
+  get "/entries" do
+    conn = Plug.Conn.fetch_query_params(conn)
+    list_name = Map.fetch!(conn.params, "list")
+    date = Date.from_iso8601!(Map.fetch!(conn.params, "date"))
+
+    entries =
+      list_name
+      |> Todo.Cache.server_process()
+      |> Todo.Server.entries(date)
+
+    formatted_entries =
+      entries
+      |> Enum.map(&"#{&1.date} #{&1.title}")
+      |> Enum.join("\n")
+
+    conn
+    |> Plug.Conn.put_resp_content_type("text/plain")
+    |> Plug.Conn.send_resp(200, formatted_entries)
+  end
+
+  match _ do
+    send_resp(conn, 404, "Oops!")
+  end
+end
